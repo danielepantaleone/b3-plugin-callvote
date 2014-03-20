10d4ddf99cbd8c3f1e4c2aa80432b879689f75c0
FenixXx <danielepantaleone@icloud.com>
2014-03-20 14:49:15 +0100

    automatically create necessary database table

--- extplugins/callvote.py
+++ extplugins/callvote.py
@@ -95,6 +95,22 @@ class CallvotePlugin(b3.plugin.Plugin):
             self.error('could not find admin plugin')
             return False
 
+        # create database tables if needed
+        tables = self.console.storage.getTables()
+        if not 'callvote' in tables:
+            self.console.storage.query("""CREATE TABLE IF NOT EXISTS `callvote` (
+                                          `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+                                          `client_id` int(11) unsigned NOT NULL,
+                                          `cv_type` varchar(20) NOT NULL,
+                                          `cv_data` varchar(40) DEFAULT NULL,
+                                          `max_num` smallint(2) unsigned NOT NULL,
+                                          `num_yes` smallint(2) unsigned NOT NULL,
+                                          `num_no` smallint(2) unsigned NOT NULL,
+                                          `time_add` int(11) unsigned NOT NULL,
+                                          PRIMARY KEY (`id`),
+                                          KEY `client_id` (`client_id`)
+                                          ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;""")
+
         # register our commands
         if 'commands' in self.config.sections():
             for cmd in self.config.options('commands'):
